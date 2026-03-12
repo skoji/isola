93695103cb1f1f22e95445b8309ab5cbe6b897fe
Satoshi Kojima <skoji@skoji.jp>
2026-03-12 21:52:05 +0900

    FileHandler now holds Hash instead of array for pages/layouts

--- lib/isola/file_handler.rb
+++ lib/isola/file_handler.rb
@@ -1,12 +1,21 @@
+def remove_ext(filepath)
+  filepath.sub(%r{\.[^/]*\z}, "")
+end
+
 module Isola
   class FileHandler
-    REJECTS = [/^_.*$/, /^~.*$/, /^\..*$/]
     attr_reader :pages, :layouts, :root_dir
-    def initialize root_dir
-      @rejects = Regexp.union(*REJECTS)
+
+    def initialize(root_dir)
+      @rejects = %r{(?:^|/)[._~]}
       @root_dir = root_dir
-      @pages = Dir.glob("**/*", base: @root_dir).reject { @rejects.match it }
-      @layouts = Dir.glob("**/*", base: File.join(@root_dir, "_layouts")).reject { @rejects.match it }
+      @pages = Dir.glob("**/*", base: @root_dir).reject { @rejects.match it }.to_h { [remove_ext(it), it] }
+      @layouts = Dir.glob("**/*", base: File.join(@root_dir, "_layouts"))
+        .reject { @rejects.match it }
+        .to_h do
+          [remove_ext(it),
+            File.join("_layouts", it)]
+      end
     end
   end
 end
--- test/test_file_handler.rb
+++ test/test_file_handler.rb
@@ -7,11 +7,11 @@ class TestFileHandler < Minitest::Test
     f = ::Isola::FileHandler.new(File.join(FIXTURES_DIR, "simple_dir"))
     files = f.pages
     assert_equal 2, files.size, files
-    assert files.include?("index.md")
-    assert files.include?("another_page.md.erb")
+    assert_equal files["index"], "index.md", files
+    assert_equal files["another_page"], "another_page.md.erb", files
 
     layouts = f.layouts
     assert_equal 1, layouts.size, layouts
-    assert_equal "default.html.erb", layouts[0]
+    assert_equal layouts["default"], "_layouts/default.html.erb", layouts
   end
 end
