e765f8bc4b3617c32a71f8fc9b8e09412674d474
Satoshi Kojima <skoji@skoji.jp>
2026-03-15 19:07:45 +0900

    fix bug in source; handling on supported file is not correct

--- lib/isola/source.rb
+++ lib/isola/source.rb
@@ -15,16 +15,16 @@ module Isola
     def render(context, site, params = {})
       path = @filepath.dup
       rendered = @content.dup
-      while !(ext = File.extname(path)).empty?
-        && site.is_supported_ext(ext)
+      while !(ext = File.extname(path)).empty? && site.is_supported_ext(ext)
         rendered = Tilt.new(path) { rendered }.render(context, params)
         path.delete_suffix! ext
+        last_ext = ext
       end
 
       if !ext.empty?
         [rendered, path]
       else
-        [rendered, path.delete_suffix(ext) + site.result_ext_for(ext)]
+        [rendered, path + site.result_ext_for(last_ext)]
       end
     end
   end
